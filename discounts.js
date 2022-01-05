e378e1be3b49aff03ba6b44f4562f8ca8945390d
Mario Rodriguez <marioalejandro130@gmail.com>
2022-01-04 20:41:36 -0600

    Created the solution for Discount Cupons chellenge. I made a select input for diferents tipes of cupons and discounts

--- discounts.js
+++ discounts.js
@@ -11,10 +11,20 @@ function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
     const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const discountCupon = inputDiscount.value;
+    
+  if(discountCupon === "15percent"){
+      var discountValue = 15;
+  } else if (discountCupon === "20percent"){
+      var discountValue = 20;
+  } else if (discountCupon === "30percent"){
+      var discountValue = 30;
+  } else if (discountCupon === "45percent"){
+      var discountValue = 45;
+  }
 
     const finalPrice = calculateDiscountPrice(priceValue, discountValue);
 
     const resultP = document.getElementById("finalPriceParagraph");
-    resultP.innerText = `Your price after discount is $${finalPrice}`
+    resultP.innerText = `Your discount is a ${discountValue}%, and yout total is $${finalPrice}`
 }
